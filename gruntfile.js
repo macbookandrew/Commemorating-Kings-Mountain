73b7e607baa7d6224bd65ddd0052109fe5258533
Andrew Minion <andrew@andrewrminion.com>
2016-08-31 09:13:45 -0400

    set up grunt

---
+++ gruntfile.js
@@ -0,0 +1,64 @@
+module.exports = function (grunt) {
+  grunt.initConfig({
+    // Watch task config
+    watch: {
+        styles: {
+            files: "SCSS/*.scss",
+            tasks: ['sass', 'postcss'],
+        },
+        javascript: {
+            files: ["js/*.js", "!js/*.min.js"],
+            tasks: ['uglify'],
+        },
+    },
+    sass: {
+        dev: {
+            files: {
+                "css/ckm.min.css" : "SCSS/ckm.scss",
+            }
+        }
+    },
+    postcss: {
+        options: {
+            map: {
+                inline: false,
+            },
+
+            processors: [
+                require('pixrem')(), // add fallbacks for rem units
+                require('autoprefixer-core')({browsers: 'last 2 versions'}), // add vendor prefixes
+                require('cssnano')() // minify the result
+            ]
+        },
+        dist: {
+            src: '*.css',
+        }
+    },
+    uglify: {
+        custom: {
+            files: {
+                'js/ckm.min.js': ['js/ckm.js'],
+            },
+        },
+    },
+    browserSync: {
+        dev: {
+            bsFiles: {
+                src : ['**/*.css', '**/*.php', '**/*.js', '!node_modules'],
+            },
+            options: {
+                watchTask: true,
+                proxy: "http://ckm.wordpress.dev",
+            },
+        },
+    },
+  });
+
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-browser-sync');
+    grunt.registerTask('default', [
+        'browserSync',
+        'watch',
+    ]);
+};
