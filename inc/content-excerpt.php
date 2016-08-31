8eb61c2357298d59aedf873cb91cd9e24e694f2e
Andrew Minion <andrew@andrewrminion.com>
2016-08-31 11:52:13 -0400

    add wrapper class to article content

--- inc/content-excerpt.php
+++ inc/content-excerpt.php
@@ -13,19 +13,21 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
     <a class="post-thumbnail" href="<?php the_permalink(); ?>" aria-hidden="true"><?php the_post_thumbnail( 'timeline-square-small' ); ?></a>
 
-    <header class="entry-header">
-        <?php
-            the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
-        ?>
-    </header><!-- .entry-header -->
+    <div class="content">
+        <header class="entry-header">
+            <?php
+                the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
+            ?>
+        </header><!-- .entry-header -->
 
-    <div class="entry-content">
-        <?php the_excerpt(); ?>
-    </div><!-- .entry-content -->
+        <div class="entry-content">
+            <?php the_excerpt(); ?>
+        </div><!-- .entry-content -->
 
-    <footer class="entry-footer">
-        <?php twentyfifteen_entry_meta(); ?>
-        <?php edit_post_link( __( 'Edit', 'twentyfifteen' ), '<span class="edit-link">', '</span>' ); ?>
-    </footer><!-- .entry-footer -->
+        <footer class="entry-footer">
+            <?php twentyfifteen_entry_meta(); ?>
+            <?php edit_post_link( __( 'Edit', 'twentyfifteen' ), '<span class="edit-link">', '</span>' ); ?>
+        </footer><!-- .entry-footer -->
+    </div>
 
 </article><!-- #post-## -->
