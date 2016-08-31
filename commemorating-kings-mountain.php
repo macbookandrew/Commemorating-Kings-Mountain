891cc6bd35b7b5d91ecea24d645951d688ab02b4
Andrew Minion <andrew@andrewrminion.com>
2016-08-31 09:15:02 -0400

    add plugin header

---
+++ commemorating-kings-mountain.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * Plugin Name: Commemorating Kings Mountain
+ * Plugin URI: https://github.com/macbookandrew/commemorating-kings-mountain/
+ * Description: Provides shortcodes for displaying historical information on a timeline
+ * Version: 1.0.0
+ * Author: AndrewRMinion Design
+ * Author URI: https://andrewrminion.com
+ * GitHub Plugin URI: https://github.com/macbookandrew/commemorating-kings-mountain/
+ */
+
+if (!defined('ABSPATH')) {
+    exit;
+}
